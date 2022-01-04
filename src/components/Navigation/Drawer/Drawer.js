574f1f009f6beab8dbca6adcd3506e903e3327ff
Dima Osadchy <osadchyd05@gmail.com>
2022-01-04 21:59:33 +0200

    first-commit

---
+++ src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,56 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, {Component} from 'react'
+import classes from './Drawer.module.css'
+import {NavLink} from 'react-router-dom'
+import BackDrop from '../../UI/BackDrop/BackDrop'
+
+const links = [
+    {to:'/', lable: 'Список', exact: true},
+    {to:'/auth', lable:'Авторизация', exact:false},
+    {to:'/quiz-creator', lable:'Создать тест', exact:false}
+]
+
+class Drawer extends Component {
+
+    clickHandler = () =>{
+        this.props.onClose()
+    }
+
+    renderLinks(){
+        return links.map((link, index) => {
+            return(
+                <li key = {index}>
+                    <NavLink
+                    to={link.to}
+                    exact={link.exact}
+                    activeClassName={classes.active}
+                    onClick ={this.clickHandler}
+                    >
+                        {link.lable}
+                    </NavLink>
+                </li>
+            )
+        })
+    }
+
+    render(){
+        const cls = [classes.Drawer]
+
+        if(!this.props.isOpen){
+            cls.push(classes.close)
+        }
+
+        return(
+            <React.Fragment>
+                    <nav className = {cls.join(' ')}>
+                <ul>
+                    {this.renderLinks()}
+                </ul>
+                    </nav>
+            { this.props.isOpen ? <BackDrop onClick = {this.props.onClose}/> :null }
+            </React.Fragment>
+        )
+    }
+}
+
+export default Drawer
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducers from "./store/reducers/rootReducers";
+import thunk from "redux-thunk";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const store = createStore(
+  rootReducers,
+  composeEnhancers(applyMiddleware(thunk))
 );
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
